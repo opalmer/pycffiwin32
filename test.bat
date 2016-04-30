24e9a0bb149c16667f3f02bbb9ba0115f270db77
Oliver Palmer <oliverpalmer@opalmer.com>
2016-04-30 18:04:23 -0400

    add test.bat

--- test.bat
+++ test.bat
@@ -5,60 +5,54 @@
 :: NOTE: This script does not perform any kind of setup (virtualenv, build
 :: tool chanin, etc)
 
-ECHO =================================
+ECHO ========================================================================================
 ECHO pep8 pywincffi
-ECHO =================================
+ECHO ========================================================================================
 pep8 pywincffi
 IF %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-ECHO =================================
+ECHO ========================================================================================
 ECHO pep8 tests
-ECHO =================================
+ECHO ========================================================================================
 pep8 tests
 IF %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-ECHO =================================
+ECHO ========================================================================================
 ECHO pylint pywincffi
-ECHO =================================
+ECHO ========================================================================================
 pylint pywincffi --reports no
 IF %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-ECHO =================================
+ECHO ========================================================================================
 ECHO pylint tests
-ECHO =================================
+ECHO ========================================================================================
 pylint tests --reports no ^
     --disable missing-docstring,invalid-name,too-many-arguments ^
     --disable protected-access,no-self-use,unused-argument ^
     --disable too-few-public-methods
 IF %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-ECHO =================================
-ECHO docs\make.bat html
-ECHO =================================
-sphinx-build -q -b html  -W -E -a -d docs/build/doctrees docs/source docs/build/html
-make.bat html
+ECHO ========================================================================================
+ECHO sphinx-build -q -b html -W -E -a -d docs/build/doctrees docs/source docs/build/html
+ECHO ========================================================================================
+sphinx-build -q -b html -W -E -a -d docs/build/doctrees docs/source docs/build/html
 IF %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-ECHO =================================
-ECHO docs\make.bat linkcheck
-ECHO =================================
-cd docs
-make.bat html
-echo %ERRORLEVEL%
+ECHO ========================================================================================
+ECHO sphinx-build -q -b linkcheck -W -E -a -d docs/build/doctrees docs/source docs/build/html
+ECHO ========================================================================================
+sphinx-build -q -b linkcheck -W -E -a -d docs/build/doctrees docs/source docs/build/html
 IF %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
-cd ..
 
-
-ECHO =================================
+ECHO ========================================================================================
 ECHO setup.py bdist_wheel
-ECHO =================================
+ECHO ========================================================================================
 python setup.py bdist_wheel > NUL
 IF %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-ECHO =================================
-ECHO nosetests -v tests
-ECHO =================================
+ECHO ========================================================================================
+ECHO nosetests -sv tests
+ECHO ========================================================================================
 nosetests -sv tests
 IF %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
-ECHO =================================
-
+ECHO ========================================================================================
