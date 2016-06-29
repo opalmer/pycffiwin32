2cca0e3ee63aa31ddafe8d9693486c69cc77b7cf
Oliver Palmer <oliverpalmer@opalmer.com>
2016-06-29 12:25:40 -0400

    removing extra header and Python functions, turns out we don't need them

--- pywincffi/core/cdefs/sources/main.c
+++ pywincffi/core/cdefs/sources/main.c
@@ -3,25 +3,6 @@
 #include <winerror.h>
 #include <windows.h>
 #include <TlHelp32.h>
-#include <Python.h>
-
-// Copied from Python's socket module.  These are used to determine
-// how PyLong_FromSocket_t should be defined.
-#ifdef MS_WIN64
-#define SIZEOF_SOCKET_T 8
-#else
-#define SIZEOF_SOCKET_T 4
-#endif
-
-// Copied from Python's socket module.  These are considered private so we
-// wouldn't have been able to used them directly.  The resulting function,
-// PyLong_FromSocket_t, is used in socket_from_fd below.
-#if SIZEOF_SOCKET_T <= SIZEOF_LONG
-#define PyLong_FromSocket_t(fd) PyLong_FromLong((SOCKET)(fd))
-#else
-#define PyLong_FromSocket_t(fd) PyLong_FromLongLong((SOCKET)(fd))
-#endif
-
 
 // Extra constants which are not defined in all versions of the Windows
 // SDK.  If cffi fails to find the value, it ends up being picked up from
@@ -51,9 +32,8 @@ HANDLE handle_from_fd(int fd) {
 }
 
 
-// Takes a file descriptor from a Python socket and converts
-// it to a Windows SOCKET object.
-HANDLE socket_from_fd(int fd) {
-    int value = (int)PyLong_FromSocket_t(fd);
-    return handle_from_fd(value);
+// Takes the file number of a socket and converts it to
+// a Windows SOCKET object.
+SOCKET socket_from_fileno(int fileno) {
+    return (SOCKET)fileno;
 }
