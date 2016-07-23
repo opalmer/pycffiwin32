5bf5563101b02b1bb350e0ffbcb77711b1331bdc
Oliver Palmer <oliverpalmer@opalmer.com>
2016-07-23 14:37:24 -0400

    minor cleanup in linters.cmd

--- .ci/appveyor/linters.cmd
+++ .ci/appveyor/linters.cmd
@@ -1,13 +1,10 @@
 @ECHO OFF
 
-%PYTHON%\\Scripts\\pep8.exe pywincffi tests
-IF %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+%PYTHON%\\Scripts\\pep8.exe pywincffi tests || EXIT 1
 
-%PYTHON%\\Scripts\\pylint.exe pywincffi --reports no
-IF %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+%PYTHON%\\Scripts\\pylint.exe pywincffi --reports no || EXIT 1
 
 %PYTHON%\\Scripts\\pylint.exe tests --reports no ^
     --disable missing-docstring,invalid-name,too-many-arguments ^
     --disable protected-access,no-self-use,unused-argument ^
-    --disable too-few-public-methods
-IF %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+    --disable too-few-public-methods || EXIT 1
