5b02b2d4ae3e38ce885e3bfbd2c1d2d22022dd0b
Oliver Palmer <oliverpalmer@opalmer.com>
2016-07-23 16:25:26 -0400

    add missing  || EXIT 1

--- .ci/appveyor/test.cmd
+++ .ci/appveyor/test.cmd
@@ -1,5 +1,5 @@
 cd C:\project
 
-%WITH_COMPILER% %PYTHON%\Scripts\nosetests.exe --with-coverage --cover-package pywincffi -v tests
-%PYTHON%\Scripts\coverage.exe xml
-%PYTHON%\Scripts\codecov.exe --required
+%WITH_COMPILER% %PYTHON%\Scripts\nosetests.exe --with-coverage --cover-package pywincffi -v tests || EXIT 1
+%PYTHON%\Scripts\coverage.exe xml || EXIT 1
+%PYTHON%\Scripts\codecov.exe --required || EXIT 1
