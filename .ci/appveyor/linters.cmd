c42472349cea8219e48de66d49e926da7db6e806
Oliver Palmer <oliverpalmer@opalmer.com>
2016-07-23 14:54:36 -0400

    disable certain checks on Python 2.6

--- .ci/appveyor/linters.cmd
+++ .ci/appveyor/linters.cmd
@@ -1,8 +1,20 @@
 @ECHO OFF
 
+IF "%PYTHON_VERSION%" == "" (
+    ECHO "PYTHON_VERSION is not set"
+    EXIT 1
+)
+
 %PYTHON%\\Scripts\\pep8.exe pywincffi tests || EXIT 1
 
-%PYTHON%\\Scripts\\pylint.exe pywincffi --reports no || EXIT 1
+:: Python < 2.6 can run without any special flags.  Python 2.6 however
+:: uses an older version of pylint which is buggy for certain checks.
+IF NOT "%PYTHON_VERSION%" == "2.6.x" (
+    %PYTHON%\\Scripts\\pylint.exe pywincffi --reports no || EXIT 1
+) ELSE (
+    %PYTHON%\\Scripts\\pylint.exe pywincffi --reports no ^
+        --disable bad-option-value,unpacking-non-sequence,maybe-no-member || EXIT 1
+)
 
 %PYTHON%\\Scripts\\pylint.exe tests --reports no ^
     --disable missing-docstring,invalid-name,too-many-arguments ^
