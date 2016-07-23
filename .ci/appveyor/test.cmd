f1108dded88c4ad1af0a1b457bc87ab99157feb9
Oliver Palmer <oliverpalmer@opalmer.com>
2016-07-23 16:20:27 -0400

    remove WITH_COMPILER in places where it's not needed

--- .ci/appveyor/test.cmd
+++ .ci/appveyor/test.cmd
@@ -1,5 +1,5 @@
 cd C:\project
 
 %WITH_COMPILER% %PYTHON%\Scripts\nosetests.exe --with-coverage --cover-package pywincffi -v tests
-%WITH_COMPILER% %PYTHON%\Scripts\coverage.exe xml
-%WITH_COMPILER% %PYTHON%\Scripts\codecov.exe --required
+%PYTHON%\Scripts\coverage.exe xml
+%PYTHON%\Scripts\codecov.exe --required
