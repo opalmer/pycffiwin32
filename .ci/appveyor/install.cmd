8cbf100c94e09167e39f18ac7c6951f5be4d3af5
Oliver Palmer <oliverpalmer@opalmer.com>
2016-07-23 15:53:59 -0400

    debugging

--- .ci/appveyor/install.cmd
+++ .ci/appveyor/install.cmd
@@ -1,16 +1,7 @@
-@ECHO OFF
+SET REQUIREMENTS="dev_requirements.txt --upgrade"
+IF "%PYTHON_VERSION%" == "2.6.x" SET REQUIREMENTS="dev_requirements-2.6.txt"
 
-IF "%PYTHON_VERSION%" == "" (
-    ECHO "PYTHON_VERSION is not set"
-    EXIT 1
-)
-
-IF NOT "%PYTHON_VERSION%" == "2.6.x" (
-    %WITH_COMPILER% %PYTHON%\\Scripts\\pip.exe install -r dev_requirements.txt --upgrade --quiet || EXIT 1
-) ELSE (
-    %WITH_COMPILER% %PYTHON%\\Scripts\\pip.exe install -r dev_requirements-2.6.txt --quiet || EXIT 1
-)
-
+%WITH_COMPILER% %PYTHON%\\Scripts\\pip.exe install -r %REQUIREMENTS% --quiet || EXIT 1
 %WITH_COMPILER% %PYTHON%\\python.exe setup.py sdist bdist_wheel bdist_msi --quiet || EXIT 1
 %WITH_COMPILER% %PYTHON%\\Scripts\\pip.exe install . --quiet || EXIT 1
 
--- .ci/appveyor/linters.cmd
+++ .ci/appveyor/linters.cmd
@@ -1,7 +1,10 @@
-IF "%PYTHON_VERSION%" == "" (
-    ECHO "PYTHON_VERSION is not set"
-    EXIT 1
-)
 
 %PYTHON%\Scripts\pep8.exe C:\project\pywincffi C:\project\tests || EXIT 1
 
+SET DISABLED_PYWINCFFI_CHECKS=""
+SET DISABLED_TEST_CHECKS="--disabled missing-docstring,invalid-name,too-many-arguments,protected-access,no-self-use,unused-argument,too-few-public-methods"
+IF "%PYTHON_VERSION%" == "2.6.x" SET DISABLED_PYWINCFFI_CHECKS="--disabled bad-option-value,unpacking-non-sequence,maybe-no-member,star-args"
+IF "%PYTHON_VERSION%" == "2.6.x" SET DISABLED_TEST_CHECKS="--disabled missing-docstring,invalid-name,too-many-arguments,protected-access,no-self-use,unused-argument,maybe-no-member,too-few-public-methods,too-many-public-methods,unpacking-non-sequence,bad-option-value,star-args,no-member,import-error"
+
+%PYTHON%\Scripts\pylint.exe C:\project\pywincffi --reports no %DISABLED_PYWINCFFI_CHECKS% || EXIT 1
+%PYTHON%\Scripts\pylint.exe C:\project\tests --reports no %DISABLED_TEST_CHECKS% || EXIT 1
