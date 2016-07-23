1673855c0c374570069688ad1dc877906ab7991c
Oliver Palmer <oliverpalmer@opalmer.com>
2016-07-23 15:07:29 -0400

    linter on Python 2.6 needs to ignore a few checks

--- .ci/appveyor/linters.cmd
+++ .ci/appveyor/linters.cmd
@@ -11,13 +11,17 @@ IF "%PYTHON_VERSION%" == "" (
 :: uses an older version of pylint which is buggy for certain checks.
 IF NOT "%PYTHON_VERSION%" == "2.6.x" (
     %PYTHON%\\Scripts\\pylint.exe pywincffi --reports no || EXIT 1
+    %PYTHON%\\Scripts\\pylint.exe tests --reports no ^
+        --disable missing-docstring,invalid-name,too-many-arguments ^
+        --disable protected-access,no-self-use,unused-argument ^
+        --disable too-few-public-methods || EXIT 1
 ) ELSE (
     %PYTHON%\\Scripts\\pylint.exe pywincffi --reports no ^
-        --disable bad-option-value,unpacking-non-sequence,maybe-no-member, ^
+        --disable bad-option-value,unpacking-non-sequence,maybe-no-member ^
         --disable star-args || EXIT 1
+    %PYTHON%\\Scripts\\pylint.exe tests --reports no ^
+        --disable missing-docstring,invalid-name,too-many-arguments ^
+        --disable protected-access,no-self-use,unused-argument,maybe-no-member ^
+        --disable too-few-public-methods,too-many-public-methods ^
+        --disable unpacking-non-sequence || EXIT 1
 )
-
-%PYTHON%\\Scripts\\pylint.exe tests --reports no ^
-    --disable missing-docstring,invalid-name,too-many-arguments ^
-    --disable protected-access,no-self-use,unused-argument ^
-    --disable too-few-public-methods || EXIT 1
