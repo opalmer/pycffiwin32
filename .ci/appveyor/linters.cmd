b418511b3ec6cbb4c6dd741089fd82c5cc89d691
Oliver Palmer <oliverpalmer@opalmer.com>
2016-07-23 15:41:31 -0400

    debugging

--- .ci/appveyor/linters.cmd
+++ .ci/appveyor/linters.cmd
@@ -3,25 +3,5 @@ IF "%PYTHON_VERSION%" == "" (
     EXIT 1
 )
 
-%PYTHON%\\Scripts\\pep8.exe pywincffi tests || EXIT 1
+%PYTHON%\Scripts\pep8.exe C:\project\pywincffi C:\project\tests || EXIT 1
 
-IF NOT "%PYTHON_VERSION%" == "2.6.x" (
-    %PYTHON%\\Scripts\\pylint.exe pywincffi --reports no || EXIT 1
-    %PYTHON%\\Scripts\\pylint.exe tests --reports no ^
-        --disable missing-docstring,invalid-name,too-many-arguments ^
-        --disable protected-access,no-self-use,unused-argument ^
-        --disable too-few-public-methods || EXIT 1
-
-:: Python 2.6's lint step is a little different.  We're using an older
-:: version of pylint which has some bugs resulting on false-positives.
-) ELSE (
-    %PYTHON%\\Scripts\\pylint.exe pywincffi --reports no ^
-        --disable bad-option-value,unpacking-non-sequence,maybe-no-member ^
-        --disable star-args || EXIT 1
-    %PYTHON%\\Scripts\\pylint.exe tests --reports no ^
-        --disable missing-docstring,invalid-name,too-many-arguments ^
-        --disable protected-access,no-self-use,unused-argument,maybe-no-member ^
-        --disable too-few-public-methods,too-many-public-methods ^
-        --disable unpacking-non-sequence,bad-option-value,star-args ^
-        --disable no-member,import-error || EXIT 1
-)
