31078aec491d8ae0d3d7a4b57350c49fdc1097ca
Oliver Palmer <oliverpalmer@opalmer.com>
2016-07-23 16:37:07 -0400

    install should only install the packages necessary to build

--- .ci/appveyor/install.cmd
+++ .ci/appveyor/install.cmd
@@ -1,6 +1,2 @@
-SET REQUIREMENTS=dev_requirements.txt --upgrade
-IF "%PYTHON_VERSION%" == "2.6.x" SET REQUIREMENTS=dev_requirements-2.6.txt
-
-%WITH_COMPILER% %PYTHON%\Scripts\pip.exe install -r %REQUIREMENTS% --quiet || EXIT 1
-%WITH_COMPILER% %PYTHON%\python.exe setup.py sdist bdist_wheel bdist_msi --quiet || EXIT 1
 %WITH_COMPILER% %PYTHON%\Scripts\pip.exe install . --quiet || EXIT 1
+%WITH_COMPILER% %PYTHON%\python.exe setup.py sdist bdist_wheel bdist_msi --quiet || EXIT 1
